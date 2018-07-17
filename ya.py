c14d7189f5c4bec15f9ccf38e9a4e85a0e5b91f2
polarfoxy <41337865+polarfoxy@users.noreply.github.com>
2018-07-17 18:35:05 +0300

    Add files via upload

---
+++ ya.py
@@ -0,0 +1,14 @@
+import pytest
+from selenium import webdriver
+
+
+
+@pytest.fixture
+def driver(request):
+    wd = webdriver.Chrome(executable_path='C:\pyton\env\chromedriver.exe')
+    request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_example(driver):
+    driver.get("http://www.ya.ru")
